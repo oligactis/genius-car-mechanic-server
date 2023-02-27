2c1955cf7a0e11d44dc9fe1c223925004257f262
Aymanoli <aymanoli777@gmail.com>
2023-02-27 18:24:56 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { MongoClient} = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 require('dotenv').config();
+
 
 const app = express();
 const port = 5000;
@@ -22,6 +24,23 @@ async function run(){
         const servicesCollection = database.collection("services");
 
         // console.log('Connected to database')
+        // Get API 
+        app.get('/services', async(req, res) =>{
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // GET single Service 
+        app.get('/services/:id', async (req, res) =>{
+            const id = req.params.id;
+            console.log('geeting specific service', id);
+            const query = {_id: new ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+
         // POST API
         app.post('/services', async(req, res) => {
             const service = req.body;
@@ -36,6 +55,16 @@ async function run(){
             console.log(result);
             res.json(result);
         })
+
+        // Delete API 
+        app.delete('/services/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
     }
     finally{
         // await client.close()
