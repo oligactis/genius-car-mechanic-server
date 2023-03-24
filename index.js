2cd54480f554a8fdf3d10d0c3dad67b16edc773c
Aymanoli <aymanoli777@gmail.com>
2023-03-25 00:05:50 +0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middlware 
 app.use(cors());
@@ -15,15 +15,13 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.far0qag.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-// , serverApi: ServerApiVersion.v1 , ServerApiVersion 
-// console.log(uri);
+
 async function run(){
     try{
         await client.connect();
         const database = client.db("carMechanic");
         const servicesCollection = database.collection("services");
 
-        // console.log('Connected to database')
         // Get API 
         app.get('/services', async(req, res) =>{
             const cursor = servicesCollection.find({});
@@ -40,17 +38,10 @@ async function run(){
             res.json(service);
         })
 
-
         // POST API
         app.post('/services', async(req, res) => {
             const service = req.body;
             console.log('hit the post api', service);
-            // const service ={
-            //     "name": "ENGINE DIAGNOSTIC",
-            //     "price": "300",
-            //     "description": "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
-            //     "img": "https://i.ibb.co/dGDkr4v/1.jpg"
-            // }
             const result = await servicesCollection.insertOne(service);
             console.log(result);
             res.json(result);
@@ -63,17 +54,13 @@ async function run(){
             const result = await servicesCollection.deleteOne(query);
             res.json(result);
         })
-
-
     }
     finally{
         // await client.close()
     }
 
 }
-run().catch(console.log('foklgj'))
-
-
+run().catch(console.dir)
 
 app.get("/", (req, res) =>{
     res.send("Runnign Geniud server hellow")
